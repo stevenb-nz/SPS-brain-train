798356588acbccf4d172ad876bf09a85da5a9b4c
Steven Brown <sgbrown@mac.com>
2019-10-18 18:49:20 +1300

    Initial Commit

---
+++ SPS brain train/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SPS brain train
+//
+//  Created by Steven Brown on 18/10/19.
+//  Copyright © 2019 Steven Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
