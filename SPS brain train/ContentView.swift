346b91659ed76daf2a27f4b1b5828b5eda546639
Steven Brown <sgbrown@mac.com>
2019-10-21 18:54:43 +1300

    basic logic working

--- SPS brain train/ContentView.swift
+++ SPS brain train/ContentView.swift
@@ -16,14 +16,24 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text(shouldWin ? "Win against" : "Lose against")
-            Text(moves[currentChoice])
-            HStack {
-                ForEach(0 ..< moves.count) {move in
-                    Button(action: {
-                        // your action here
-                    }) {
-                        Text(self.moves[move])
+            Section {
+                HStack {
+                    Text(shouldWin ? "Win against " : "Lose against ")
+                    Text(moves[currentChoice])
+                }
+            }
+            Section {
+                HStack {
+                    ForEach(0 ..< moves.count) {move in
+                        Button(action: {
+                            if (self.shouldWin && (self.currentChoice == (move < 2 ? move+1 : 0))) || (!self.shouldWin && (self.currentChoice == (move > 0 ? move-1 : 2))) {
+                                print("correct")
+                            }
+                            self.currentChoice = Int.random(in: 0...2)
+                            self.shouldWin = Bool.random()
+                        }) {
+                            Text(self.moves[move])
+                        }
                     }
                 }
             }
